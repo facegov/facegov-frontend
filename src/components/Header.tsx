a74796f312d931148354ec9a8c9115479cb5c956
Emanuele <puglieseweb@gmail.com>
2024-09-22 23:31:10 +0100

    refactored

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,30 +12,39 @@ interface NavigationProps {
 const Header: React.FC<NavigationProps> = ({isMobileMenuOpen, setIsMobileMenuOpen}) => (
     <>
         <nav className="bg-blue-600 p-3 text-white">
-            <div className="container mx-auto flex justify-between items-center">
-                <Link to="/" className="flex items-center">
-                    <LogoIcon className="w-8 h-8"/>
-                </Link>
-                <h1 className="text-3xl font-bold text-white">FaceGov</h1>
+            <div className="container mx-auto px-4">
+                <div className="flex items-center">
+                    <div className="w-1/3 flex justify-start">
+                        <Link to="/" className="flex items-center">
+                            <LogoIcon className="w-8 h-8"/>
+                        </Link>
+                    </div>
 
-                <div className="hidden md:flex space-x-4">
-                    <Menu/>
-                    {/*<Icons name="search"/>*/}
-                    {/*<Icons name="bell"/>*/}
-                    {/*<Link to="/login" className="flex items-center">*/}
-                    {/*    <Icons name="login"/>*/}
-                    {/*</Link>*/}
-                    {/*<Link to="/about" className="flex items-center">*/}
-                    {/*    <Icons name="about"/>*/}
-                    {/*</Link>*/}
-                    {/*<LanguageSwitcher/>*/}
+                    <div className="w-1/3 flex justify-center">
+                        <h1 className="text-3xl font-bold text-white">FaceGov</h1>
+                    </div>
+
+                    <div className="w-1/3 flex justify-end">
+                        <div className="hidden md:flex space-x-4">
+                            <Menu/>
+                            {/*<Icons name="search"/>*/}
+                            {/*<Icons name="bell"/>*/}
+                            {/*<Link to="/login" className="flex items-center">*/}
+                            {/*    <Icons name="login"/>*/}
+                            {/*</Link>*/}
+                            {/*<Link to="/about" className="flex items-center">*/}
+                            {/*    <Icons name="about"/>*/}
+                            {/*</Link>*/}
+                            {/*<LanguageSwitcher/>*/}
+                        </div>
+                        <button
+                            className="md:hidden"
+                            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+                        >
+                            <Icons name="menu"/>
+                        </button>
+                    </div>
                 </div>
-                <button
-                    className="md:hidden"
-                    onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-                >
-                    <Icons name="menu"/>
-                </button>
             </div>
         </nav>
 

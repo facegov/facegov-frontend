082bfe28cf94608cc574597edeb079d9d1e8f046
Emanuele <puglieseweb@gmail.com>
2024-09-22 21:30:03 +0100

    refactored

--- src/pages/community/TrendingTopicsPage.tsx
+++ src/pages/community/TrendingTopicsPage.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import PrototypeDisclaimer from "@/components/PrototypeDisclaimer";
 import CommunityMenu from "@/pages/community/CommunityMenu";
 

e554eef006ad19d9a3a179116824f7eb09489b25
Emanuele <puglieseweb@gmail.com>
2024-09-22 21:27:33 +0100

    refactored

--- src/pages/community/TrendingTopicsPage.tsx
+++ src/pages/community/TrendingTopicsPage.tsx
@@ -3,11 +3,6 @@ import PrototypeDisclaimer from "@/components/PrototypeDisclaimer";
 import CommunityMenu from "@/pages/community/CommunityMenu";
 
 const TrendingTopics: React.FC = () => {
-    const [threads] = useState([
-        {id: 1, title: 'Climate Change Policy', author: 'EcoWarrior', replies: 23},
-        {id: 2, title: 'Education Reform Ideas', author: 'FutureTeacher', replies: 15},
-        {id: 3, title: 'Healthcare Accessibility', author: 'DocPolicy', replies: 37},
-    ]);
 
     return (
         <div className="container mx-auto mt-8">

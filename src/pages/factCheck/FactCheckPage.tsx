f8b456d1f8672221b876edb393c19b7d02acad93
Emanuele <puglieseweb@gmail.com>
2024-09-23 10:24:42 +0100

    Added select Your coutry screen

--- src/pages/factCheck/FactCheckPage.tsx
+++ src/pages/factCheck/FactCheckPage.tsx
@@ -10,9 +10,8 @@ const FactCheckPage: React.FC = () => {
             <h1 className="text-4xl font-bold text-blue-600 mb-4">Fact Check</h1>
             <FactCheckMenu/>
             <div className="mb-8 text-center text-gray-700">
-                <p>Welcome to our Political Fact Check page. In an era of information overload,
-                    we're here to help you navigate the complex world of political claims and statements.
-                    Our team of experts rigorously investigates and verifies political assertions to
+                <p>Navigate the complex world of political claims and statements.
+                    Fact Check aims to verify political assertions and
                     provide you with accurate, unbiased information.</p>
 
             </div>
--- src/pages/politicians/PoliticiansPage.tsx
+++ src/pages/politicians/PoliticiansPage.tsx
@@ -8,9 +8,7 @@ const PoliticiansPage: React.FC = () => {
             <h1 className="text-4xl font-bold text-blue-600 mb-4">Politicians</h1>
             <PoliticiansMenu/>
             <div className="mb-8 text-center text-gray-700">
-                In an era of digital connectivity, this platform offers a unique
-                opportunity for elected officials and political candidates to engage directly with constituents. Share
-                your vision, address concerns, and collaborate with citizens to develop policies that truly reflect the
+                Share your vision, address concerns, and collaborate with citizens to develop policies that truly reflect the
                 needs of your community. Transparency builds trust, and dialogue fosters progress. Use this space to
                 demonstrate your commitment to open governance and responsive leadership. Let&apos;s work together to
                 strengthen our democracy, one conversation at a time.
